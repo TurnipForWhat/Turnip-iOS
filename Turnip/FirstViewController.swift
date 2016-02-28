b2b22e9b43e67c8b8dd65449bd3a9e78e671907a
Tyler Hoyt <tylerhoyt93@gmail.com>
2016-02-28 11:01:10 -0500

    Added default code

---
+++ Turnip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Turnip
+//
+//  Created by Tyler Hoyt on 2/26/16.
+//  Copyright © 2016 Turnip Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
