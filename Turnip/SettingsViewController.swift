dd14230f02c50751516d59baaae90baf4d413d4c
Tyler Hoyt <tylerhoyt93@gmail.com>
2016-04-11 22:18:25 -0400

    Basic skeletion of settings page

---
+++ Turnip/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Turnip
+//
+//  Created by Tyler Hoyt on 4/11/16.
+//  Copyright © 2016 Turnip Apps. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
